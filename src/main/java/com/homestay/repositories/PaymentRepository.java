769eb40459f0c4fde9833dd10f589fed1768451f
NguyenXuanMinh22 <nguyenxuanminh289@gmail.com>
2025-03-23 23:05:56 +0700

    Add files via upload

--- src/main/java/com/homestay/repositories/PaymentRepository.java
+++ src/main/java/com/homestay/repositories/PaymentRepository.java
@@ -5,3 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    List<Payment> findByStatus(String status); // Tìm các thanh toán theo trạng thái
+}
